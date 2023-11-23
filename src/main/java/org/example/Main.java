fee8728df6ba15332b3c15d78fcd627fa62f6c8d
CardGame_FaroukNemanja <FaroukIzzDaa@hotmail.com>
2023-11-23 15:18:53 +0100

    Aufgabe 1 started

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-
         //Aufgabe 1
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben Sie ihren Namen ein:");
+        String firstname = scanner.nextLine();
+        System.out.println("Geben Sie ihren Nickname ein:");
+        String nickname = scanner.nextLine();
+
+        String fullname = firstname.concat(nickname);
+        System.out.println(fullname);
+        System.out.println(fullname.toUpperCase());
+
+
     }
 }
